7fe0e226e923ed91e685e5d604460b05e3249d18
neandrake <die.drachen@gmail.com>
2015-11-11 03:17:56 -0500

    Accidentally removed wrong file, had incorrect name

--- extensions/ManiphestExcelDefaultIncludeCustomFieldsFormat.php
+++ extensions/ManiphestExcelDefaultIncludeCustomFieldsFormat.php
@@ -16,6 +16,10 @@
  *   It was not clear what the appropriate manner for retrieving this data is
  *   so it is more likely to break with upgrades to Phabricator.
  *
+ *   The only custom field types tested are the following:
+ *    - PhabricatorStandardCustomFieldInt
+ *    - PhabricatorStandardCustomFieldSelect
+ *
  *   Another issues is that *_NO_* policy-checking is done on the fields - 
  *   It wasn't clear whether this was the case, but PhabricatorCustomField
  *   does seem to have some methods for setting/requiring a "viewer".
@@ -40,92 +44,104 @@ final class ManiphestExcelDefaultIncludeCustomFieldsFormat extends ManiphestExce
     $sheet = $workbook->setActiveSheetIndex(0);
     $sheet->setTitle(pht('Tasks'));
 
+    // Header Cell
+    // title => the displayed header title in the spreadsheet, in row 0
+    // width => initial width in pixels for the column, null leaves unspecified
+    // celltype => which format the column data should be set as, default is STRING
+    //   can be null if it's a date field
+    // isDate => there is no date format in the PHPExcel_Cell_DataType, so this is needed
+    // cftype => the custom field data type, only specified for custom field headers
+
     $colHeaders = array(
       array(
-      	'title' => pht('ID'),
-      	'width' => null,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('ID'),
+        'width' => null,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('Owner'),
-      	'width' => 15,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('Owner'),
+        'width' => 15,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('Status'),
-      	'width' => null,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('Status'),
+        'width' => null,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('Priority'),
-      	'width' => 10,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('Priority'),
+        'width' => 10,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('Date Created'),
-      	'width' => 15,
-      	'celltype' => null,
-      	'isDate' => true,
+        'title' => pht('Date Created'),
+        'width' => 15,
+        'celltype' => null,
+        'isDate' => true,
       ),
       array(
-      	'title' => pht('Date Updated'),
-      	'width' => 15,
-      	'celltype' => null,
-      	'isDate' => true,
+        'title' => pht('Date Updated'),
+        'width' => 15,
+        'celltype' => null,
+        'isDate' => true,
       ),
       array(
-      	'title' => pht('Title'),
-      	'width' => 60,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('Title'),
+        'width' => 60,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('Projects'),
-      	'width' => 20,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('Projects'),
+        'width' => 20,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
       array(
-      	'title' => pht('URI'),
-      	'width' => 30,
-      	'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-      	'isDate' => false,
+        'title' => pht('URI'),
+        'width' => 30,
+        'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+        'isDate' => false,
       ),
     );
 
+    // Create the custom fields from their configured definition and extract header cell details
     $customFields = id(new ManiphestConfiguredCustomField())->createFields(null);
-    $customFieldsMap = array();
+    $customFieldsHeaderMap = array();
     foreach ($customFields as $customField) {
-    	$fieldName = $customField->getProxy()->getFieldName();
-    	$fieldType = $customField->getProxy()->getFieldType();
+      // Using the proxy I think is needed due to using ManiphestConfiguredCustomField.createFields
+      // That seems to be wrapping the PhabricatorStandardCustomField, but doesn't proxy/delegate the
+      // getFieldType() method which is needed here.
+      $fieldName = $customField->getProxy()->getFieldName();
+      $fieldType = $customField->getProxy()->getFieldType();
 
-    	$isDateField = $fieldType == 'date';
-    	$cellType = PHPExcel_Cell_DataType::TYPE_STRING;
-    	if ($fieldType == 'int') {
-    		$cellType = PHPExcel_Cell_DataType::TYPE_NUMERIC;
-    	}
+      $isDateField = $fieldType == 'date';
+      $cellType = PHPExcel_Cell_DataType::TYPE_STRING;
+      if ($fieldType == 'int') {
+        $cellType = PHPExcel_Cell_DataType::TYPE_NUMERIC;
+      }
 
-    	$customFieldHeader = array(
-    	  'title' => $fieldName,
-    	  'width' => null,
-    	  'celltype' => $cellType,
-    	  'isDate' => $isDateField,
-    	  'cftype' => $fieldType,
-    	);
-    	$customFieldsMap[$fieldName] = $customFieldHeader;
-    	$colHeaders[] = $customFieldHeader;
+      $customFieldHeader = array(
+        'title' => $fieldName,
+        'width' => null,
+        'celltype' => $cellType,
+        'isDate' => $isDateField,
+        'cftype' => $fieldType,
+      );
+      $customFieldsHeaderMap[$fieldName] = $customFieldHeader;
+      $colHeaders[] = $customFieldHeader;
     }
 
     $colHeaders[] = array(
       'title' => pht('Description'),
-  	  'width' => 100,
-  	  'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
-  	  'isDate' => false,
-  	);
+      'width' => 100,
+      'celltype' => PHPExcel_Cell_DataType::TYPE_STRING,
+      'isDate' => false,
+    );
 
     $status_map = ManiphestTaskStatus::getTaskStatusMap();
     $pri_map = ManiphestTaskPriority::getTaskPriorityMap();
@@ -140,7 +156,7 @@ final class ManiphestExcelDefaultIncludeCustomFieldsFormat extends ManiphestExce
 
     $headerRow = array();
     foreach ($colHeaders as $colIdx => $column) {
-	  $headerRow[] = $column['title'];
+    $headerRow[] = $column['title'];
     }
     $rows[] = $headerRow;
 
@@ -168,27 +184,34 @@ final class ManiphestExcelDefaultIncludeCustomFieldsFormat extends ManiphestExce
         PhabricatorEnv::getProductionURI('/T'.$task->getID()),
       );
 
+      // Query for the custom fields for a specific maniphest task object
       $taskCustomFields = PhabricatorCustomField::getObjectFields($task, PhabricatorCustomField::ROLE_DEFAULT);
       $taskCustomFields->readFieldsFromStorage($task);
       $taskCustomFieldsMap = array();
       foreach ($taskCustomFields->getFields() as $customField) {
-      	$fieldName = $customField->getFieldName();
-      	$taskCustomFieldsMap[$fieldName] = $customField;
+        $fieldName = $customField->getFieldName();
+        $taskCustomFieldsMap[$fieldName] = $customField;
       }
-      // loop over the task's custom fields in the same order they appear in the header
-      foreach ($customFieldsMap as $fieldName => $customFieldHeader) {
-      	$customField = $taskCustomFieldsMap[$fieldName];
-      	if ($customField == null) {
-      		$row[] = null;
-      		continue;
-      	}
 
-      	$fieldValue = $customField->getProxy()->getFieldValue();
-      	if ($fieldValue !== null && $customFieldHeader['cftype'] == 'select') {
-      		$options = $customField->getProxy()->getOptions();
-      		$fieldValue = $options[$fieldValue];
-      	}
-		    $row[] = $fieldValue;
+      // We want to order the items from the task custom field object in the same order
+      // which the custom field headers exist in the $colHeaders array
+      // So loop over the header map, and pull the populated custom field from the populated map
+      foreach ($customFieldsHeaderMap as $fieldName => $customFieldHeader) {
+        $customField = $taskCustomFieldsMap[$fieldName];
+        if ($customField == null) {
+          $row[] = null;
+          continue;
+        }
+
+        $fieldValue = $customField->getProxy()->getFieldValue();
+
+        // option/select-style custom fields have values which are actually the identifier from json spec
+        // lookup the display value to be used from the 'getOptions()' on the PhabricatorStandardCustomFieldSelect
+        if ($fieldValue !== null && $customFieldHeader['cftype'] == 'select') {
+          $options = $customField->getProxy()->getOptions();
+          $fieldValue = $options[$fieldValue];
+        }
+        $row[] = $fieldValue;
       }
 
       $row[] = id(new PhutilUTF8StringTruncator())
@@ -204,28 +227,30 @@ final class ManiphestExcelDefaultIncludeCustomFieldsFormat extends ManiphestExce
         $cell = $sheet
           ->setCellValue($cell_name, $spec, $return_cell = true);
 
+        // If the header row only apply the bold-style and width, but do not 
+        // apply the date-format/data-type since the values will always be string
         if ($row == 0) {
           $sheet->getStyle($cell_name)->applyFromArray($header_format);
 
           $width = $colHeaders[$col]['width'];
           if ($width !== null) {
-          	$sheet->getColumnDimension($this->col($col))->setWidth($width);
+            $sheet->getColumnDimension($this->col($col))->setWidth($width);
           }
         } else {
-        	$is_date = $colHeaders[$col]['isDate'];
-	        if ($is_date) {
-	          $code = PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2;
-	          $sheet
-	            ->getStyle($cell_name)
-	            ->getNumberFormat()
-	            ->setFormatCode($code);
-	        } else {
-	          $cellType = $colHeaders[$col]['celltype'];
-	          if ($cellType == null) {
-	          	$cellType = PHPExcel_Cell_DataType::TYPE_STRING;
-	          }
-	          $cell->setDataType($cellType);
-	        }
+          $is_date = $colHeaders[$col]['isDate'];
+          if ($is_date) {
+            $code = PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2;
+            $sheet
+              ->getStyle($cell_name)
+              ->getNumberFormat()
+              ->setFormatCode($code);
+          } else {
+            $cellType = $colHeaders[$col]['celltype'];
+            if ($cellType == null) {
+              $cellType = PHPExcel_Cell_DataType::TYPE_STRING;
+            }
+            $cell->setDataType($cellType);
+          }
         }
       }
     }
